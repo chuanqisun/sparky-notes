75439f6e0bd0327776e40cd9f8b3ddd192fe3db0
Chuanqi <chusun@microsoft.com>
2022-08-05 16:24:37 -0700

    search hits and show content

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -1,35 +1,56 @@
 // This widget will open an Iframe window with buttons to show a toast message and close the window.
 
 const { widget } = figma;
-const { useEffect, Text } = widget;
+const { useEffect, useSyncedState, Text, AutoLayout } = widget;
 
 function Widget() {
+  const [content, setContent] = useSyncedState<null | string>("cardContent", null);
+
   useEffect(() => {
     figma.ui.onmessage = (msg) => {
-      if (msg.type === "showToast") {
-        figma.notify("Hello widget");
-      }
-      if (msg.type === "close") {
-        figma.closePlugin();
+      if (msg.type === "selectItem") {
+        const { id, title } = msg;
+
+        figma.notify(`${title} added to canvas`);
+        setContent(title);
       }
     };
   });
 
   return (
-    <Text
-      fontSize={24}
-      onClick={
-        // Use async callbacks or return a promise to keep the Iframe window
-        // opened. Resolving the promise, closing the Iframe window, or calling
-        // "figma.closePlugin()" will terminate the code.
-        () =>
-          new Promise((resolve) => {
-            figma.showUI(__html__);
-          })
-      }
-    >
-      Open IFrame
-    </Text>
+    <AutoLayout>
+      <Text
+        fontSize={24}
+        onClick={
+          // Use async callbacks or return a promise to keep the Iframe window
+          // opened. Resolving the promise, closing the Iframe window, or calling
+          // "figma.closePlugin()" will terminate the code.
+          () =>
+            new Promise((resolve) => {
+              figma.showUI(__html__, {
+                width: 480,
+                height: 720,
+              });
+            })
+        }
+      >
+        {content ? content : "Search in HITS"}
+      </Text>
+      <Text
+        fontSize={24}
+        onClick={
+          // Use async callbacks or return a promise to keep the Iframe window
+          // opened. Resolving the promise, closing the Iframe window, or calling
+          // "figma.closePlugin()" will terminate the code.
+          () =>
+            new Promise((resolve) => {
+              figma.showUI(`<script>window.location.href="https://hits.microsoft.com"</script>`);
+            })
+        }
+      >
+        Show HITS
+      </Text>
+    </AutoLayout>
   );
 }
 
--- src/ui/ui.ts
+++ src/ui/ui.ts
@@ -1,3 +1,47 @@
-console.log("hello from UI ");
+async function main() {
+  const tokenInput = document.querySelector(`input[name="token"]`) as HTMLInputElement;
+  const searchButton = document.getElementById("search") as HTMLButtonElement;
 
-export default {};
+  const query = `query Search($args: SearchArgs!) {
+    search(args: $args) {
+      organicResults {
+        id
+        title
+      }
+    }
+  }`;
+
+  searchButton.onclick = async () => {
+    const data = await fetch("https://hits-uat.microsoft.com/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "hits-api-base-url": "https://hitsstage-api-uat.azurewebsites.net/api/",
+        "hits-api-authorization": `Bearer ${tokenInput.value}`,
+      },
+      body: JSON.stringify({
+        query,
+        variables: {
+          args: {
+            query: "Keyboard",
+            filters: {},
+          },
+        },
+      }),
+    }).then((r) => r.json());
+
+    document.getElementById("results")!.innerHTML = (data as any).data.search.organicResults
+      .map((result) => `<li><button data-id="${result.id}" data-title="${result.title}">${result.id} - ${result.title}</button></li>`)
+      .join("");
+  };
+
+  document.getElementById("results")!.onclick = (e) => {
+    const item = (e.target as HTMLElement).closest("[data-id]")!;
+    const id = item.getAttribute("data-id");
+    const title = item.getAttribute("data-title");
+    parent.postMessage({ pluginMessage: { type: "selectItem", id, title } }, "*");
+  };
+}
+
+main();
