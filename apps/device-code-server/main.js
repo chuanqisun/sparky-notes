bf7dc2c2bc9ea0328b0ee0b0933c1fb7bb1c26dc
chuanqi <chusun@microsoft.com>
2022-08-08 15:21:52 -0700

    add search api

--- apps/device-code-server/main.js
+++ apps/device-code-server/main.js
@@ -1,14 +1,18 @@
 const axios = require("axios");
 const qs = require("qs");
 
+const { search } = require("./graphql");
+
 const AAD_CLIENT_ID = "bc9d8487-53f6-418d-bdce-7ed1f265c33a";
 const AAD_TENANT_ID = "72f988bf-86f1-41af-91ab-2d7cd011db47";
 const HITS_API_RESOURCE_ID = "https://microsoft.onmicrosoft.com/MSFT_HITS_API";
 
 async function main() {
+  // test graphql access
+
   const requestData = qs.stringify({
     client_id: AAD_CLIENT_ID,
-    scope: "user.read offline_access",
+    scope: `${HITS_API_RESOURCE_ID}/.default offline_access`,
   });
   const requestConfig = {
     method: "post",
@@ -45,7 +49,7 @@ async function main() {
         console.log(pollResponse.data);
         clearInterval(poller);
         clearTimeout(timeout);
-        resolve(pollResponse);
+        resolve(pollResponse.data);
       } catch {}
     }, 5000);
 
@@ -57,6 +61,9 @@ async function main() {
 
   console.log(authResult);
   const { access_token, refresh_token } = authResult;
+
+  const searchResult = await search({ phrase: "gaming", token: access_token });
+  console.log(searchResult);
 }
 
 main();
