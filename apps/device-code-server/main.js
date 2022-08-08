69fd5aeb3a83c2daca4f4ea16c3b12bd85670fae
chuanqi <chusun@microsoft.com>
2022-08-08 14:53:46 -0700

    refactor auth

--- apps/device-code-server/main.js
+++ apps/device-code-server/main.js
@@ -1,14 +1,18 @@
 const axios = require("axios");
 const qs = require("qs");
 
+const AAD_CLIENT_ID = "bc9d8487-53f6-418d-bdce-7ed1f265c33a";
+const AAD_TENANT_ID = "72f988bf-86f1-41af-91ab-2d7cd011db47";
+const HITS_API_RESOURCE_ID = "https://microsoft.onmicrosoft.com/MSFT_HITS_API";
+
 async function main() {
   const requestData = qs.stringify({
-    client_id: "325dce49-3946-473a-9427-cd186fa462c2",
+    client_id: AAD_CLIENT_ID,
     scope: "user.read offline_access",
   });
   const requestConfig = {
     method: "post",
-    url: "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/v2.0/devicecode",
+    url: `https://login.microsoftonline.com/${AAD_TENANT_ID}/oauth2/v2.0/devicecode`,
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
     },
@@ -21,27 +25,38 @@ async function main() {
   const pollData = qs.stringify({
     grant_type: "urn:ietf:params:oauth:grant-type:device_code",
     code: requestResult.device_code,
-    client_id: "325dce49-3946-473a-9427-cd186fa462c2",
+    client_id: AAD_CLIENT_ID,
   });
   const pollConfig = {
     method: "post",
-    url: "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/v2.0/token",
+    url: `https://login.microsoftonline.com/${AAD_TENANT_ID}/oauth2/v2.0/token`,
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
     },
     data: pollData,
   };
 
-  setInterval(async () => {
-    console.log("polling...");
+  const authResult = await new Promise((resolve, reject) => {
+    const poller = setInterval(async () => {
+      console.log("polling...");
 
-    try {
-      const pollResponse = await axios(pollConfig);
-      console.log(pollResponse.data);
-    } catch {
-      console.log("no response yet");
-    }
-  }, 5000);
+      try {
+        const pollResponse = await axios(pollConfig);
+        console.log(pollResponse.data);
+        clearInterval(poller);
+        clearTimeout(timeout);
+        resolve(pollResponse);
+      } catch {}
+    }, 5000);
+
+    const timeout = setTimeout(() => {
+      clearInterval(poller);
+      reject("Timeout");
+    }, 120000); // 2 min
+  });
+
+  console.log(authResult);
+  const { access_token, refresh_token } = authResult;
 }
 
 main();
