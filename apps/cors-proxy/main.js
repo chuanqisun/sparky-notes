8c1c4fc04022f700a4f53613f75111034d5c10c8
chuanqi <chusun@microsoft.com>
2022-08-09 14:58:21 -0700

    add cors proxy

--- apps/cors-proxy/main.js
+++ apps/cors-proxy/main.js
@@ -2,9 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const AAD_CLIENT_ID = "bc9d8487-53f6-418d-bdce-7ed1f265c33a";
 const AAD_TENANT_ID = "72f988bf-86f1-41af-91ab-2d7cd011db47";
-const HITS_API_RESOURCE_ID = "https://microsoft.onmicrosoft.com/MSFT_HITS_API";
 
 const port = process.env.PORT || 5000;
 const app = express();
