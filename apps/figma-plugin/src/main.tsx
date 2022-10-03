8bfdbc2cc4bed8bdaa75e5f972ef6c3f94a9faf7
chuanqi <chusun@microsoft.com>
2022-10-03 15:44:14 -0700

    adjust iframe

--- apps/figma-plugin/src/main.tsx
+++ apps/figma-plugin/src/main.tsx
@@ -1,5 +1,8 @@
 async function main() {
-  figma.showUI(`<script>window.location.href="${process.env.WEB_URL}"</script>`);
+  figma.showUI(`<script>window.location.href="${process.env.WEB_URL}"</script>`, {
+    height: 800,
+    width: 420,
+  });
   figma.ui.onmessage = (msg) => {};
 }
 
