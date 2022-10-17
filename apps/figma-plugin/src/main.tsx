5fb89982b0077de4a4479ca42219ed8b865979a6
chuanqi <chusun@microsoft.com>
2022-10-17 15:25:10 -0700

    convert to widget

--- apps/figma-plugin/src/main.tsx
+++ apps/figma-plugin/src/main.tsx
@@ -1,83 +1,102 @@
 import { MessageToMain } from "@h20/types";
+const { widget } = figma;
+const { useEffect, Text } = widget;
 
-async function main() {
+const showUI = () =>
   figma.showUI(`<script>window.location.href="${process.env.WEB_URL}"</script>`, {
     height: 800,
     width: 420,
   });
 
-  figma.ui.onmessage = async (msg: MessageToMain) => {
-    console.log(msg);
+function Widget() {
+  useEffect(() => {
+    figma.ui.onmessage = async (msg: MessageToMain) => {
+      console.log(msg);
 
-    if (msg.importResult) {
-      if (msg.importResult.isSuccess) {
-        figma.notify("Import HITS Search success");
-      } else {
-        figma.notify("Import HITS Search failed", { error: true });
+      if (msg.importResult) {
+        if (msg.importResult.isSuccess) {
+          figma.notify("Import HITS Search success");
+        } else {
+          figma.notify("Import HITS Search failed", { error: true });
+        }
       }
-    }
 
-    if (msg.addCard) {
-      await figma.loadFontAsync({ family: "Inter", style: "Regular" });
-      const textNode = figma.createText();
-      textNode.characters = msg.addCard.title;
-      if (msg.addCard.url) {
-        textNode.hyperlink = {
-          type: "URL",
-          value: msg.addCard.url,
+      if (msg.addCard) {
+        await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+        const textNode = figma.createText();
+        textNode.characters = msg.addCard.title;
+        if (msg.addCard.url) {
+          textNode.hyperlink = {
+            type: "URL",
+            value: msg.addCard.url,
+          };
+        }
+
+        const frame = figma.createFrame();
+        frame.layoutMode = "VERTICAL";
+        frame.appendChild(textNode);
+
+        frame.paddingTop = 8;
+        frame.paddingBottom = 8;
+        frame.paddingLeft = 16;
+        frame.paddingRight = 16;
+        frame.cornerRadius = 8;
+
+        frame.counterAxisSizingMode = "AUTO";
+        frame.primaryAxisSizingMode = "AUTO";
+
+        const blackPaint: SolidPaint = {
+          type: "SOLID",
+          color: {
+            r: 0,
+            g: 0,
+            b: 0,
+          },
         };
+        const whitePaint: SolidPaint = {
+          type: "SOLID",
+          color: {
+            r: 1,
+            g: 1,
+            b: 1,
+          },
+        };
+        // paint.paints = [solidPaint]:
+        frame.backgrounds = [whitePaint];
+        frame.strokes = [blackPaint];
+        frame.strokeWeight = 2;
+
+        textNode.resize(400, 10);
+        textNode.textAutoResize = "HEIGHT";
+        textNode.layoutAlign = "STRETCH";
+        textNode.fontSize = 20;
+        textNode.fills = [blackPaint];
+        textNode.locked = true;
+
+        frame.x = figma.viewport.center.x - frame.width / 2 + 32;
+        frame.y = figma.viewport.center.y - frame.height / 2 + 32;
+        figma.currentPage.appendChild(frame);
+
+        figma.viewport.scrollAndZoomIntoView([frame]);
+
+        figma.notify(`✅ Card added to canvas`);
       }
+    };
+  });
 
-      const frame = figma.createFrame();
-      frame.layoutMode = "VERTICAL";
-      frame.appendChild(textNode);
-
-      frame.paddingTop = 8;
-      frame.paddingBottom = 8;
-      frame.paddingLeft = 16;
-      frame.paddingRight = 16;
-      frame.cornerRadius = 8;
-
-      frame.counterAxisSizingMode = "AUTO";
-      frame.primaryAxisSizingMode = "AUTO";
-
-      const blackPaint: SolidPaint = {
-        type: "SOLID",
-        color: {
-          r: 0,
-          g: 0,
-          b: 0,
-        },
-      };
-      const whitePaint: SolidPaint = {
-        type: "SOLID",
-        color: {
-          r: 1,
-          g: 1,
-          b: 1,
-        },
-      };
-      // paint.paints = [solidPaint]:
-      frame.backgrounds = [whitePaint];
-      frame.strokes = [blackPaint];
-      frame.strokeWeight = 2;
-
-      textNode.resize(400, 10);
-      textNode.textAutoResize = "HEIGHT";
-      textNode.layoutAlign = "STRETCH";
-      textNode.fontSize = 20;
-      textNode.fills = [blackPaint];
-      textNode.locked = true;
-
-      frame.x = figma.viewport.center.x - frame.width / 2 + 32;
-      frame.y = figma.viewport.center.y - frame.height / 2 + 32;
-      figma.currentPage.appendChild(frame);
-
-      figma.viewport.scrollAndZoomIntoView([frame]);
-
-      figma.notify(`✅ Card added to canvas`);
-    }
-  };
+  return (
+    <Text
+      fontSize={24}
+      onClick={
+        // Use async callbacks or return a promise to keep the Iframe window
+        // opened. Resolving the promise, closing the Iframe window, or calling
+        // "figma.closePlugin()" will terminate the code.
+        () => new Promise((resolve) => showUI())
+      }
+    >
+      Open IFrame
+    </Text>
+  );
 }
 
-main();
+widget.register(Widget);
