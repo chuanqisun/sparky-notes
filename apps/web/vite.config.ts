71a842e6670f7f71f0a0c2def4a56cc2af907ec9
chuanqisun <chusun@microsoft.com>
2022-11-01 02:58:13 +0000

    fix missing routes

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,4 +1,5 @@
 import preact from "@preact/preset-vite";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -6,4 +7,14 @@ export default defineConfig({
   clearScreen: false,
   base: "",
   plugins: [preact()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        debug: resolve(__dirname, "debug.html"),
+        signIn: resolve(__dirname, "sign-in.html"),
+        authRedirect: resolve(__dirname, "auth-redirect.html"),
+      },
+    },
+  },
 });
