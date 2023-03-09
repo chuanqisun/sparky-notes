7d32098df42f2862241e1dcb8046dbb86cbfddeb
chuanqisun <chusun@microsoft.com>
2023-03-09 00:20:28 +0000

    add vite

---
+++ apps/impromptu-web/src/modules/auth/use-config.ts
@@ -0,0 +1,11 @@
+import { useLocalStorage } from "../../utils/use-local-storage";
+import { CONFIG_CACHE_KEY, getInitialConfig } from "./config";
+
+export function useConfig() {
+  const hitsConfig = useLocalStorage({
+    key: CONFIG_CACHE_KEY,
+    getInitialValue: getInitialConfig,
+  });
+
+  return hitsConfig;
+}
