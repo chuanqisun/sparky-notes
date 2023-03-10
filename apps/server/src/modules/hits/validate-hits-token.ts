9c0f4ceeaa8acf1f138d5988fa239ae1d61baa2b
chuanqisun <chusun@microsoft.com>
2023-03-10 13:48:39 -0800

    add hits token cache

--- apps/server/src/modules/hits/validate-hits-token.ts
+++ apps/server/src/modules/hits/validate-hits-token.ts
@@ -4,6 +4,8 @@ import type { RequestHandler } from "express";
 import { constants } from "http2";
 import { performance } from "perf_hooks";
 
+const inMemoryTokenCache = new Map<string, number>();
+
 export const validateHitsToken: RequestHandler = async (req, res, next) => {
   const authorizationStr = req.header("Authorization");
   if (!authorizationStr) {
@@ -13,6 +15,13 @@ export const validateHitsToken: RequestHandler = async (req, res, next) => {
 
   try {
     performance.mark("start");
+
+    const cachedExpiry = inMemoryTokenCache.get(authorizationStr);
+    if (cachedExpiry && cachedExpiry > Date.now()) {
+      next();
+      return;
+    }
+
     const response = await axios({
       method: "post",
       url: `https://hits.microsoft.com/api/search/index`,
@@ -29,6 +38,16 @@ export const validateHitsToken: RequestHandler = async (req, res, next) => {
     assert(response.status === 200);
     assert(Array.isArray(response.data.results) && response.data.results.length === 0);
     console.log(`[token] validated ${performance.measure("Auth overhead", "start").duration.toFixed(2)} ms`);
+
+    inMemoryTokenCache.set(authorizationStr, Date.now() + 1000 * 60 * 5); // good for 5 minutes
+
+    // remove all other expired tokens
+    [...inMemoryTokenCache.keys()].forEach((key) => {
+      if (inMemoryTokenCache.get(key)! < Date.now()) {
+        inMemoryTokenCache.delete(key);
+      }
+    });
+
     next();
   } catch (e) {
     next(e);
