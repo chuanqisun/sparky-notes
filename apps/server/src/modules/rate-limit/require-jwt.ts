d6b267c62b51ad291b93cc7663ca067c0e7fddd5
chuanqisun <chusun@microsoft.com>
2023-03-10 10:12:30 -0800

    add debug log

--- apps/server/src/modules/rate-limit/require-jwt.ts
+++ apps/server/src/modules/rate-limit/require-jwt.ts
@@ -2,7 +2,11 @@ import type { RequestHandler } from "express";
 import { throttle } from "../../utils/throttle";
 
 export const rateLimit: (rpm: number, margin?: number) => RequestHandler = (rpm, margin = 0.05) => {
-  const tick = () => Promise.resolve();
+  let count = 0;
+  const tick = () => {
+    console.log(count++);
+    return Promise.resolve();
+  };
   const throttledTick = throttle(tick, ((1 + margin) * 60 * 1000) / rpm);
 
   return async (req, res, next) => {
