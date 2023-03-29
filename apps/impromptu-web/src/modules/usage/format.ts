215ff02749de67b87c5cc5ae5fa047cfd1e0c00b
chusun <chusun@microsoft.com>
2023-03-29 00:06:40 +0000

    adjust meter behavior

--- apps/impromptu-web/src/modules/usage/format.ts
+++ apps/impromptu-web/src/modules/usage/format.ts
@@ -1,11 +1,12 @@
 export function formatLargeNumber(number: number) {
+  const original = number;
   const units = ["", "K", "M", "B", "T"];
   let unitIndex = 0;
   while (number >= 1000 && unitIndex < units.length - 1) {
     number /= 1000;
     unitIndex++;
   }
-  const roundedNumber = number > 1000 ? number.toFixed(1) : number;
+  const roundedNumber = original < 1000 ? number : number.toFixed(1);
   const unit = units[unitIndex];
   return roundedNumber + unit;
 }
