2118344218267224d4602df64d314ed25c39b8f3
chusun <chusun@microsoft.com>
2023-04-07 04:29:36 +0000

    drag boundary

--- apps/symphony-lab/src/studio/graph/node-element.ts
+++ apps/symphony-lab/src/studio/graph/node-element.ts
@@ -23,36 +23,44 @@ export class NodeElement extends HTMLElement {
   private elementOrigin: { x: number; y: number } = { x: 0, y: 0 };
 
   connectedCallback() {
-    this.elementOrigin = { x: this.getBoundingClientRect().x, y: this.getBoundingClientRect().y };
+    this.handleMousemove = this.handleMousemove.bind(this);
+    this.cancelDrag = this.cancelDrag.bind(this);
 
-    const handleMousemove = (e: Event) => {
-      this.dispatchEvent(
-        new CustomEvent<NodeDraggedEventInit>("node-dragged", {
-          detail: {
-            target: this,
-            x: (e as MouseEvent).x - this.pointerStartPosition.x + this.currentTransform.x,
-            y: (e as MouseEvent).y - this.pointerStartPosition.y + this.currentTransform.y,
-            width: this.currentTransform.width,
-            height: this.currentTransform.height,
-          },
-          bubbles: true,
-        })
-      );
-    };
+    this.elementOrigin = { x: this.getBoundingClientRect().x, y: this.getBoundingClientRect().y };
 
     this.dragBar.addEventListener("pointerdown", (e) => {
       const rect = this.getBoundingClientRect();
       this.currentTransform = { x: rect.x - this.elementOrigin.x, y: rect.y - this.elementOrigin.y, width: rect.width, height: rect.height };
       this.pointerStartPosition = { x: e.x, y: e.y };
-      window.addEventListener("pointermove", handleMousemove);
+      this.dragBar.dataset.active = "true";
+      window.addEventListener("pointermove", this.handleMousemove);
     });
 
-    this.dragBar.addEventListener("pointerup", (e) => {
-      window.removeEventListener("pointermove", handleMousemove);
-    });
+    window.addEventListener("pointerup", this.cancelDrag);
   }
 
   translate2d(x: number, y: number) {
     this.style.transform = `translate3d(${x}px, ${y}px, 0)`;
+  }
+
+  private handleMousemove = (e: Event) => {
+    this.dispatchEvent(
+      new CustomEvent<NodeDraggedEventInit>("node-dragged", {
+        detail: {
+          target: this,
+          x: (e as MouseEvent).x - this.pointerStartPosition.x + this.currentTransform.x,
+          y: (e as MouseEvent).y - this.pointerStartPosition.y + this.currentTransform.y,
+          width: this.currentTransform.width,
+          height: this.currentTransform.height,
+        },
+        bubbles: true,
+      })
+    );
+  };
+
+  cancelDrag() {
+    window.removeEventListener("pointermove", this.handleMousemove);
+
+    delete this.dragBar.dataset.active;
   }
 }
