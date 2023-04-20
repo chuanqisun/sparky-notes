d1bac5c9f1ec44d7bee73f5b86d6d2c6647ef6e4
chusun <chusun@microsoft.com>
2023-04-19 17:39:31 -0700

    context node

--- packages/symphony-types/index.d.ts
+++ packages/symphony-types/index.d.ts
@@ -1,21 +1,11 @@
 export interface MessageToWeb {
-  upstreamGraphChanged?: LiveProgram[];
-  respondAmbientPrograms?: DisplayProgram[];
-  respondContextPath?: DisplayProgram[][];
-  respondCreateProgram?: DisplayProgram;
-  respondRuntimeUpdate?: boolean;
-  respondUpstreamGraph?: DisplayProgram[];
+  upstreamGraphChanged?: OperatorNode[];
+  respondUpstreamGraph?: OperatorNode[];
 }
 
 // messages starting with "request" must be handled with "respond"
 export interface MessageToFigma {
   notifyCreateDebugOperator?: CreateDebugOperatorInput;
-  requestAmbientPrograms?: ViewportNodesInput;
-  requestContextPath?: string; // includes nodes before and above the selected node
-  requestCreateProgram?: CreateProgramInput;
-  requestCreateSerialTaskNodes?: CreateSerialTaskNodesInput;
-  requestCreateSpatialProgram?: CreateSpatialProgramInput;
-  requestRemoveDownstreamNode?: string;
   requestUpstreamGraph?: {
     leafIds: string[];
   };
@@ -34,52 +24,10 @@ export interface CreateDebugOperatorInput {
   data: any[];
 }
 
-export interface GraphSelection {
-  nodeName: string;
-}
-
-export interface DisplayProgram {
-  id: string;
-  subtype: string;
-  input: string;
-  context: string;
-  dirFromAnchor: SpatialDirection;
-}
-
 export interface OperatorNode {
   id: string;
   name: string;
   config: Record<string, any>;
   data: any[];
-}
-
-export interface LiverOperatorNode extends OperatorNode {}
-
-export interface LiveProgram extends DisplayProgram {
-  isSelected: boolean;
-}
-
-export interface CreateSerialTaskNodesInput {
-  parentId: string;
-  taskDescriptions: string[];
-}
-
-export interface CreateSpatialProgramInput {
-  anchorId?: string;
-  context?: string;
-  directionFromAnchor?: SpatialDirection;
-  subtype: string;
-  input: string;
-}
-
-export interface CreateProgramInput {
-  parentIds: string[];
-  subtype: string;
-  input: string;
-}
-
-export type SpatialDirection = "Up" | "Down" | "Left" | "Right";
-
-export interface ViewportNodesInput {
-  anchorIds: string[];
+  isSelected?: boolean;
 }
