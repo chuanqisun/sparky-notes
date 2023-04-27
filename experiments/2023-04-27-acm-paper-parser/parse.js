3be72f0023a393d6538b9587f29bfc39db611950
chusun <chusun@microsoft.com>
2023-04-27 15:03:37 -0700

    update parse script

--- experiments/2023-04-27-acm-paper-parser/parse.js
+++ experiments/2023-04-27-acm-paper-parser/parse.js
@@ -1,7 +1,15 @@
 let bibtex = "";
+
+// bib to json
 let result = JSON.stringify(
   bibtex
     .split(`@inproceedings`)
     .filter(Boolean)
     .map((paper) => Object.fromEntries([...paper.matchAll(/(\w+) = \{(.+?)\}/gim)].map((matchInstance) => matchInstance.slice(1))))
 );
+
+// json to text
+JSON.parse(result)
+  .map((item) => Object.fromEntries(Object.entries(item).filter(([key, value]) => ["title", "url", "abstract"].includes(key))))
+  .map((item) => `Title: ${item.title}\nAbstract: ${item.abstract}\nURL: ${item.url}`)
+  .join("\n\n");
