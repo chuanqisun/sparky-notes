640ecabd2b140b584b7967c41f0a8ed934b9ec4c
chusun <chusun@microsoft.com>
2023-05-01 13:15:43 -0700

    update bidi query

--- apps/data-loader/src/lib/repl/handlers/entity-query.ts
+++ apps/data-loader/src/lib/repl/handlers/entity-query.ts
@@ -19,6 +19,11 @@ export async function entityQueryHandler(db: CozoDb, command: string) {
         .filter(Boolean)
     );
 
+  if (!fromKeywords?.length || !toKeywords?.length) {
+    console.log(`❌ Invalid format. Pattern: "e: <keyword>, <keyword> -> <keyword>, <keyword>"`);
+    return;
+  }
+
   const allKeywords = [...new Set([...fromKeywords, ...toKeywords])];
 
   // todo boost perf with cache
@@ -61,65 +66,35 @@ export async function entityQueryHandler(db: CozoDb, command: string) {
     )
     .catch((e) => console.log(e?.display ?? e));
 
-  console.log(rawResult.rows.map((row: any[]) => [row[2], ...row[3]].join(",")));
-
   for (const row of rawResult.rows) {
     const entities = row[3];
-    const minigraph = [];
+    const minigraph: string[] = [];
 
     for (let i = 0; i < entities.length - 1; i++) {
       const fromE = entities[i];
       const toE = entities[i + 1];
-      const forwardPredicates = await joinWithPredicateEdge(fromE, toE);
-      const backwardPredicates = await joinWithPredicateEdge(toE, fromE);
-      const isSimilarEntityPair = await isSimilar(db, 0.2, fromE, toE);
+      // const forwardPredicates = await joinWithPredicateEdge(db, fromE, toE);
+      // const backwardPredicates = await joinWithPredicateEdge(db, toE, fromE);
+      // const isSimilarEntityPair = await isSimilar(db, 0.2, fromE, toE);
 
-      minigraph.push(...forwardPredicates.map((p) => `(${fromE})-[${p}]->(${toE})`));
-      minigraph.push(...backwardPredicates.map((p) => `(${toE})-[${p}]->(${fromE})`));
-      minigraph.push(...(isSimilarEntityPair ? [`(${fromE})-[similar to]->(${toE})`] : []));
+      const bidiEdges = await getBidiPredicateEdge(db, fromE, toE);
+      console.log(bidiEdges);
+
+      // minigraph.push(...forwardPredicates.map((p) => `(${fromE})-[${p}]->(${toE})`));
+      // minigraph.push(...backwardPredicates.map((p) => `(${toE})-[${p}]->(${fromE})`));
+      // minigraph.push(...(isSimilarEntityPair ? [`(${fromE})-[similar to]->(${toE})`] : []));
     }
 
     console.log("---");
     console.log(minigraph.join("\n"));
   }
-
-  async function joinWithPredicateEdge(fromE: string, toE: string): Promise<string[]> {
-    const raw = await db.run(
-      `
-      hardEdge[s, o] <- [[$fromE, $toE]]
-      ?[s, p, o] := *claimTriple{s, p, o}, hardEdge[s, o]
-`,
-      {
-        fromE,
-        toE,
-      }
-    );
-
-    return raw.rows.map((row: string[]) => row[1]);
-  }
 }
 
-async function keywordToEntites(db: CozoDb, keyword: string): Promise<string[]> {
-  // keyword to vec
-  const [vec] = await bulkGetEmbeddings([keyword]);
-
-  // vector search nearest neighbors
-
-  await db
-    .run(
-      `
-?[text, dist] := ~entity:semantic{text | query: vec($vec), k: 10, ef: 16, bind_distance: dist, radius: 0.2 }
-
-:sort dist
-  `,
-      {
-        vec,
-      }
-    )
-    .then(console.log)
-    .catch((e) => console.log(e?.display ?? e));
-
-  return [];
+export interface OntologyGraphEdge {
+  fromNode: string;
+  toNode: string;
+  type: "predicate" | "similarity";
+  predicate?: string;
 }
 
 async function isSimilar(db: CozoDb, threshold: number, fromE: string, toE: string): Promise<Boolean> {
@@ -147,4 +122,49 @@ async function getL2Distance(db: CozoDb, fromText: string, toText: string): Prom
     });
 
   return result;
+}
+
+async function joinWithPredicateEdge(db: CozoDb, fromE: string, toE: string): Promise<string[]> {
+  const raw = await db.run(
+    `
+    hardEdge[s, o] <- [[$fromE, $toE]]
+    ?[s, p, o] := *claimTriple{s, p, o}, hardEdge[s, o]
+`,
+    {
+      fromE,
+      toE,
+    }
+  );
+
+  return raw.rows.map((row: string[]) => row[1]);
+}
+
+interface PredicateEdge {
+  claimId: string;
+  subject: string;
+  predicate: string;
+  object: string;
+}
+async function getBidiPredicateEdge(db: CozoDb, fromE: string, toE: string): Promise<PredicateEdge[]> {
+  const raw = await db
+    .run(
+      `
+    forwardEdge[claimId, from, p, to] := *claimTriple{claimId, s: $fromE, p, o: $toE}, from = $fromE, to = $toE
+    backwardEdge[claimId, to, p, from] := *claimTriple{claimId, s: $toE, p, o: $fromE}, from = $fromE, to = $toE
+    ?[from, p, to, claimId, claimTitle] := forwardEdge[claimId, from, p, to] or backwardEdge[claimId, from, p, to], *claim{claimId, claimTitle}
+`,
+      {
+        fromE,
+        toE,
+      }
+    )
+    .catch((e) => console.log(e.display));
+
+  return raw.rows.map((row: string[]) => ({
+    subject: row[0],
+    predicate: row[1],
+    object: row[2],
+    claimId: row[3],
+    claimTitle: row[4],
+  }));
 }
