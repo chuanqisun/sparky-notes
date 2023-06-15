187dfb5b1716c20e349d93822b330c200f1ece50
chusun <chusun@microsoft.com>
2023-06-15 23:46:34 +0000

    refactor

--- apps/symphony-lab/src/flow/custom-node/shared/title-bar.tsx
+++ apps/symphony-lab/src/flow/custom-node/shared/title-bar.tsx
@@ -13,9 +13,9 @@ export const TitleBar = (props: TitleBarProps) => {
     <DragBar>
       {props.title}
       <div>
+        <button onClick={props.onRun}>Run</button>
         <button onClick={() => props.onSetDebug(!props.isDebug)}>Trace</button>
         <button onClick={() => props.maxTargetRef.current?.requestFullscreen()}>Max</button>
-        <button onClick={props.onRun}>Run</button>
         <button onClick={props.onClear}>Clear</button>
       </div>
     </DragBar>
