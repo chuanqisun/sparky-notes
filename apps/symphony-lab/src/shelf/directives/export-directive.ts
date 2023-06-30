b248dfeb640ac1c80bf06836b5ebbd17bc5c63fa
chusun <chusun@microsoft.com>
2023-06-30 00:43:29 +0000

    fix export issue

--- apps/symphony-lab/src/shelf/directives/export-directive.ts
+++ apps/symphony-lab/src/shelf/directives/export-directive.ts
@@ -4,7 +4,7 @@ export function createExportDirective(): ShelfDirective {
   return {
     match: (source) => source.startsWith("/export"),
     run: async ({ updateStatus: setStatus, data }) => {
-      const [fileHandle] = (await (window as any).showSaveFilePicker()) as FileSystemFileHandle[];
+      const fileHandle = (await (window as any).showSaveFilePicker()) as FileSystemFileHandle;
       const file = await fileHandle.createWritable();
       await file.write(JSON.stringify(data));
       await file.close();
