43a64da53cf7254760d6aed444890e68629b3d7b
chusun <chusun@microsoft.com>
2023-07-23 03:01:36 +0000

    simplify feedback loop

--- apps/symphony-lab/src/jq/feedback-loop.ts
+++ apps/symphony-lab/src/jq/feedback-loop.ts
@@ -1,24 +1,8 @@
-interface IteratorConfig<TGoal, TPlan, TResult, TFeedback> {
-  goal: TGoal;
-  plan?: TPlan;
-  result?: TResult;
-  feedback?: TFeedback;
-  onPlan: (goal: TGoal, plan?: TPlan, result?: TResult, feedback?: TFeedback) => Promise<TPlan> | TPlan;
-  onRun: (plan: TPlan) => Promise<TResult> | TResult;
-  onEval: (goal: TGoal, plan: TPlan, result: TResult) => Promise<TFeedback> | TFeedback;
-  onExit: (goal: TGoal, result: TResult, feedback: TFeedback) => boolean;
-}
-
-async function iterate<TGoal, TPlan, TResult, TFeedback>(config: IteratorConfig<TGoal, TPlan, TResult, TFeedback>) {
-  const { goal, plan: prevPlan, result: prevResult, feedback: prevFeedback } = config;
-
-  const newPlan = await config.onPlan(goal, prevPlan, prevResult, prevFeedback);
-  const newResult = await config.onRun(newPlan);
-  const newFeedback = await config.onEval(goal, newPlan, newResult);
-
-  if (config.onExit(goal, newResult, newFeedback)) {
-    return { plan: newPlan, result: newResult, feedback: newFeedback };
-  } else {
-    return iterate({ ...config, goal, plan: newPlan, result: newResult, feedback: newFeedback });
+async function feedbackLoop<T>(runFn: (previousError?: any) => T, retryLeft = 3, previousError?: any) {
+  if (retryLeft <= 0) throw new Error("Failed to converge");
+  try {
+    return await runFn(previousError);
+  } catch (e: any) {
+    return feedbackLoop(runFn, retryLeft - 1, e);
   }
 }
