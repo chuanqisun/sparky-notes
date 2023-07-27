f3e09feb4e975db415d3e412ec893562a948c71f
chuanqisun <chusun@microsoft.com>
2023-07-27 05:14:25 +0000

    add interpreter skeleton

--- apps/symphony-lab/src/motif-lang/interpret.ts
+++ apps/symphony-lab/src/motif-lang/interpret.ts
@@ -5,4 +5,25 @@ export interface SystemApi {
   notifyProgress: (progress: number, total: number) => void;
 }
 
-export async function interpret(program: MotifProgram, lib: any) {}
+export interface RunContext {
+  system: SystemApi;
+}
+export interface LibraryFunction {
+  operator: string;
+  description?: string;
+  run: (data: any[], operand: string, context: RunContext) => Promise<void>;
+}
+
+export async function interpret(program: MotifProgram, libFunctions: Record<string, LibraryFunction>, data: any[]) {
+  for (const statement of program.statements) {
+    const libFunc = libFunctions[statement.operator];
+    if (!libFunc) throw new Error(`Unknown operator: "${statement.operator}"`);
+
+    await libFunc.run(data, statement.operand, {
+      system: {
+        addItems: (...items) => console.log("TODO, implement add items", items),
+        notifyProgress: (progress, total) => console.log("TODO, implement notify progress", progress, total),
+      },
+    });
+  }
+}
