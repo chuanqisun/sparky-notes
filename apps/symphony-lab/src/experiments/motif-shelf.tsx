7e2712ee51aedc67b0e7d0b59d8352ec4d9746d9
chusun <chusun@microsoft.com>
2023-07-27 20:54:56 +0000

    working search

--- apps/symphony-lab/src/experiments/motif-shelf.tsx
+++ apps/symphony-lab/src/experiments/motif-shelf.tsx
@@ -1,22 +1,127 @@
+import { ChatManager, ChatWorker, getOpenAIWorkerProxy } from "@h20/plex-chat";
+import { getTimeoutFunction } from "@h20/plex-chat/src/controller/timeout";
+import { LogLevel } from "@h20/plex-chat/src/scheduler/logger";
 import type React from "react";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { JSONTree } from "react-json-tree";
 import styled from "styled-components";
+import { useAuthContext } from "../account/auth-context";
+import { useModelSelector } from "../account/model-selector";
 import { AutoResize } from "../form/auto-resize";
+import { getH20Proxy } from "../hits/proxy";
+import { getSemanticSearchProxy } from "../hits/search-claims";
 import { parseProgram } from "../motif-lang/compiler";
 import { run, type Runtime } from "../motif-lang/runtime";
 import { hitsSearch } from "../motif-lib/hits/search";
 import { useMotifShelfManager } from "../motif-shelf/use-motif-shelf-manager";
+import type { ChatProxy, FnCallProxy, RawProxy } from "../openai/chat";
+import { defaultModelConfig, defaultModels } from "../openai/config";
+import { estimateChatTokenDemand } from "../openai/tokens";
 import { StyledOutput, theme } from "../shelf/json-view";
 import { CenterClamp } from "../shell/center-clamp";
 
 export interface MotifShelfProps {}
 
+const modelIdToTokenLimit = (modelId: string) => {
+  switch (modelId) {
+    case "gpt-35-turbo":
+      return {
+        rpm: 720,
+        tpm: 120_000,
+        timeout: getTimeoutFunction(3_000, 25),
+        contextWindow: 8_192,
+      };
+    case "gpt-35-turbo-16k":
+      return {
+        rpm: 522,
+        tpm: 87_000,
+        timeout: getTimeoutFunction(3_000, 25),
+        contextWindow: 16_384,
+      };
+    case "gpt-4":
+      return {
+        rpm: 60,
+        tpm: 10_000,
+        timeout: getTimeoutFunction(5_000, 30),
+        contextWindow: 8_192,
+      };
+    case "gpt-4-32k":
+      return {
+        rpm: 180,
+        tpm: 30_000,
+        timeout: getTimeoutFunction(5_000, 30),
+        contextWindow: 32_768,
+      };
+    default:
+      throw new Error(`Unknown model id: ${modelId}`);
+  }
+};
+
 export const MotifShelf: React.FC<MotifShelfProps> = () => {
   const { userMessage, updateUserMessage, shelves, openShelf, currentShelf, updateShelfData } = useMotifShelfManager();
   const [status, setStatus] = useState("Ready");
 
-  const plugins = useMemo(() => [hitsSearch()], []);
+  const { accessToken } = useAuthContext();
+
+  const { allChatEndpoints } = useModelSelector();
+
+  const chatManager = useMemo(() => {
+    console.log("endpoints", allChatEndpoints);
+
+    const workers = allChatEndpoints.map((endpoint) => {
+      const limits = modelIdToTokenLimit(endpoint.modelDisplayName);
+      return new ChatWorker({
+        proxy: getOpenAIWorkerProxy({
+          apiKey: endpoint.apiKey,
+          endpoint: endpoint.endpoint,
+        }),
+        models: [endpoint.modelDisplayName],
+        concurrency: 10,
+        timeout: limits.timeout,
+        requestsPerMinute: limits.rpm,
+        tokensPerMinute: limits.tpm,
+        contextWindow: limits.contextWindow,
+        logLevel: LogLevel.Warn,
+      });
+    });
+
+    return new ChatManager({ workers, logLevel: LogLevel.Info });
+  }, [allChatEndpoints]);
+
+  const completionCall = useCallback<RawProxy>(
+    (messages, modelConfig) => {
+      if (!chatManager) throw new Error("No chat manager");
+
+      const { models, ...restConfig } = { models: defaultModels, ...modelConfig };
+
+      const finalInput = {
+        ...defaultModelConfig,
+        ...restConfig,
+        messages,
+      };
+
+      return chatManager.submit({
+        tokenDemand: estimateChatTokenDemand(finalInput),
+        models,
+        input: finalInput,
+      });
+    },
+    [chatManager]
+  );
+  const fnCall = useCallback<FnCallProxy>(
+    (messages, modelConfig) => completionCall(messages, modelConfig).then((rawOutput) => rawOutput.choices[0].message.function_call!),
+    [completionCall]
+  );
+
+  const chat = useCallback<ChatProxy>(
+    (messages, modelConfig) => completionCall(messages, modelConfig).then((rawOutput) => rawOutput.choices[0].message.content ?? ""),
+    [completionCall]
+  );
+
+  const h20Proxy = getH20Proxy(accessToken);
+  const semanticSearchProxy = useMemo(() => getSemanticSearchProxy(h20Proxy), [h20Proxy]);
+
+  const plugins = useMemo(() => [hitsSearch(fnCall, semanticSearchProxy)], [fnCall, semanticSearchProxy]);
 
   const handleSubmit = useCallback(async () => {
     console.log("submitted", userMessage);
--- apps/symphony-lab/src/motif-lib/hits/search.ts
+++ apps/symphony-lab/src/motif-lib/hits/search.ts
@@ -1,9 +1,16 @@
+import { getSemanticSearchInput, type SemanticSearchProxy } from "../../hits/search-claims";
 import type { Plugin } from "../../motif-lang/runtime";
+import type { FnCallProxy } from "../../openai/chat";
 
-export function hitsSearch(): Plugin {
+export function hitsSearch(fnCallProxy: FnCallProxy, semanticSearchProxy: SemanticSearchProxy): Plugin {
   return {
     operator: "/hits/search",
     description: "Search HITS for UX insights",
-    run: async (data, operand, context) => {},
+    run: async (data, operand, context) => {
+      // parse operand into params: query, limit
+      const searchResults = await semanticSearchProxy(getSemanticSearchInput(operand, 50));
+
+      context.addItems(...(searchResults.value ?? []));
+    },
   };
 }
