efb5f057dc35c0130ed663d908d70ee48933b9bd
chuanqisun <chusun@microsoft.com>
2023-08-11 23:19:32 +0000

    refactor

--- apps/assistant/plugin/src/handlers/handle-drop-cards.ts
+++ apps/assistant/plugin/src/handlers/handle-drop-cards.ts
@@ -3,52 +3,33 @@ import { loadFonts, replaceNotification } from "@h20/figma-tools";
 import { startLayoutDraftFrame } from "../utils/layout";
 import type { LayoutDraft } from "../widget/use-layout-draft";
 
+const VERTICAL_GAP = 32;
+
 export async function handleDropCards(message: MessageToFigma, currentNodeId: string, widgetManifestId: string) {
   if (!message.createCards) return;
 
   const summary = message.createCards;
   const { figmaDropContext, webDragContext } = summary;
 
-  await loadFonts({ family: "Inter", style: "Medium" }, { family: "Inter", style: "Semi Bold" });
-
-  let cloneFromNode = figma.getNodeById(currentNodeId) as WidgetNode;
-
+  const cloneFromNode = getCloneFromNode(currentNodeId, widgetManifestId);
   if (!cloneFromNode) {
-    cloneFromNode = figma.currentPage
-      .findAllWithCriteria({ types: ["WIDGET"] })
-      .filter((node) => node.widgetId === widgetManifestId)
-      .pop() as WidgetNode;
-
-    if (!cloneFromNode) {
-      replaceNotification("Widget no longer exists", { error: true });
-    }
-  }
-
-  if (!figmaDropContext) {
-    const primaryCard = summary.cards[0]!;
-    const clonedWidget = cloneFromNode.cloneWidget({ cardData: primaryCard });
-    replaceNotification(`✅ Card added to canvas`, {
-      button: {
-        text: "Locate it",
-        action: () => figma.viewport.scrollAndZoomIntoView([clonedWidget]),
-      },
-    });
-
-    figma.currentPage.appendChild(clonedWidget);
-    clonedWidget.x = cloneFromNode.x;
-    clonedWidget.y = cloneFromNode.y + cloneFromNode.height + 32;
+    replaceNotification("Widget no longer exists", { error: true });
     return;
   }
 
-  const parentNode = figma.getNodeById(figmaDropContext.parentNodeId);
-  if (!Array.isArray((parentNode as ChildrenMixin)?.children)) return;
+  await loadFonts({ family: "Inter", style: "Medium" }, { family: "Inter", style: "Semi Bold" });
 
-  const frame = startLayoutDraftFrame({ layoutMode: "VERTICAL", itemSpacing: 32 });
+  const frame = startLayoutDraftFrame({ layoutMode: "VERTICAL", itemSpacing: VERTICAL_GAP });
+  frame.x = figmaDropContext?.x ?? cloneFromNode.x;
+  frame.y = figmaDropContext?.y ?? cloneFromNode.y + cloneFromNode.height + VERTICAL_GAP;
+
+  const parentNode = figmaDropContext ? figma.getNodeById(figmaDropContext.parentNodeId) : figma.currentPage;
+  if (!Array.isArray((parentNode as ChildrenMixin)?.children)) {
+    replaceNotification("This container cannot be used for dropping", { error: true });
+    return;
+  }
 
   (parentNode as ChildrenMixin).appendChild(frame);
-
-  frame.x = figmaDropContext.x;
-  frame.y = figmaDropContext.y;
 
   summary.cards.forEach((card, index) => {
     const clonedWidget = cloneFromNode.cloneWidget({ cardData: card });
@@ -67,5 +48,27 @@ export async function handleDropCards(message: MessageToFigma, currentNodeId: st
         layoutDraft,
       });
     }
+
+    if (!figmaDropContext) {
+      replaceNotification(`✅ Card added to canvas`, {
+        button: {
+          text: "Locate it",
+          action: () => figma.viewport.scrollAndZoomIntoView([clonedWidget]),
+        },
+      });
+    }
   });
+}
+
+function getCloneFromNode(currentNodeId: string, widgetManifestId: string): WidgetNode | null {
+  let cloneFromNode = figma.getNodeById(currentNodeId) as WidgetNode | null;
+
+  if (!cloneFromNode) {
+    cloneFromNode = figma.currentPage
+      .findAllWithCriteria({ types: ["WIDGET"] })
+      .filter((node) => node.widgetId === widgetManifestId)
+      .pop() as WidgetNode;
+  }
+
+  return cloneFromNode;
 }
