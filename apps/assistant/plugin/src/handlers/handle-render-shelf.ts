d9bb5782debcc3541131930b00301227849cfdec
chusun <chusun@microsoft.com>
2023-08-19 01:27:44 +0000

    optimized render layout

--- apps/assistant/plugin/src/handlers/handle-render-shelf.ts
+++ apps/assistant/plugin/src/handlers/handle-render-shelf.ts
@@ -7,19 +7,20 @@ export async function handleRenderShelf(message: MessageToFigma) {
 
   await loadFonts({ family: "Inter", style: "Medium" });
 
-  const result = renderObjectRecursively({ [message.renderShelf.name]: JSON.parse(message.renderShelf.rawData) });
+  const displayNodes = getDisplayNodes({ _root: JSON.parse(message.renderShelf.rawData) });
+  const renderedRoot = renderDisplayNodes(displayNodes)[0]! as SectionNode;
+  const renderedItems = renderedRoot.children as SceneNode[];
+  figma.ungroup(renderedRoot);
 
-  console.log(getDisplayNodes({ [message.renderShelf.name]: JSON.parse(message.renderShelf.rawData) }));
-
-  moveToViewCenter(result);
-  figma.currentPage.selection = result;
+  moveToViewCenter(renderedItems);
+  figma.currentPage.selection = renderedItems;
 }
 
 interface DisplaySection {
   type: "Section";
   name: string;
   children: (DisplaySection | DisplaySticky)[];
-  direction: "vertical" | "horizontal";
+  direction: "Vertical" | "Horizontal";
 }
 interface DisplaySticky {
   type: "Sticky";
@@ -37,66 +38,106 @@ function getDisplayNodes(data: any): (DisplaySection | DisplaySticky)[] {
   }
 
   if (Array.isArray(data)) {
-    const stickies: DisplaySticky[] = data.filter(isPrimitive).map((item) => ({
-      type: "Sticky",
-      text: item.toString(),
-    }));
+    const { stickies, sections } = data.reduce<{
+      stickies: DisplaySticky[];
+      sections: DisplaySection[];
+      currentIndex: 0;
+    }>(
+      (results, value) => {
+        if (isPrimitive(value)) {
+          results.stickies.push({
+            type: "Sticky",
+            text: value.toString(),
+          });
+        } else if (isNamedGroup(value)) {
+          const children = getDisplayNodes(value.items);
+          results.sections.push({
+            type: "Section",
+            name: value.name,
+            children,
+            direction: getDirection(children),
+          });
+        } else {
+          const children = getDisplayNodes(value);
 
-    const Sections: DisplaySection[] = data
-      .filter((value) => !isPrimitive(value))
-      .map((value, index) => ({
-        type: "Section",
-        name: index.toString(),
-        children: getDisplayNodes(value),
-        direction: "horizontal",
-      }));
+          results.sections.push({
+            type: "Section",
+            name: results.currentIndex.toString(),
+            children,
+            direction: getDirection(children),
+          });
 
-    return [...stickies, ...Sections];
+          results.currentIndex++;
+        }
+
+        return results;
+      },
+      {
+        stickies: [],
+        sections: [],
+        currentIndex: 0,
+      }
+    );
+
+    return [...stickies, ...sections];
   }
 
   if (typeof data === "object") {
-    return Object.entries(data).map(([key, value]) => ({
-      type: "Section",
-      name: key,
-      children: getDisplayNodes(value),
-      direction: "vertical",
-    }));
+    return Object.entries(data).map(([key, value]) => {
+      const children = getDisplayNodes(value);
+
+      return {
+        type: "Section",
+        name: key,
+        children,
+        direction: getDirection(children),
+      };
+    });
   }
 
   return [];
 }
 
-function renderDisplayNodes(nodes: (DisplaySection | DisplaySticky)[]) {
-  const stickies = nodes.filter((node) => node.type === "Sticky");
+function isNamedGroup(node: any): node is {
+  name: string;
+  items: any[];
+} {
+  return typeof node.name === "string" && Array.isArray(node.items);
 }
 
-function renderStiky(node: DisplaySticky) {
+function getDirection(nodes: (DisplaySection | DisplaySticky)[]) {
+  return nodes.every((node) => node.type === "Sticky") ? "Horizontal" : "Vertical";
+}
+
+function renderDisplayNodes(nodes: (DisplaySection | DisplaySticky)[]): SceneNode[] {
+  const stickies = nodes.filter(isDisplaySticky).map(renderSticky);
+  const sections = nodes.filter(isDisplaySection).map(renderSection);
+
+  return [...stickies, ...sections];
+}
+
+function renderSection(node: DisplaySection): SceneNode {
+  const sectionItems = renderDisplayNodes(node.children);
+  const section = figma.createSection();
+
+  section.name = node.name;
+  const layoutFn = node.direction === "Horizontal" ? getNextHorizontalTilePosition : getNextVerticalTilePosition;
+  appendAsTiles(section, sectionItems, layoutFn);
+
+  return section;
+}
+
+function renderSticky(node: DisplaySticky): SceneNode {
   const sticky = figma.createSticky();
   sticky.text.characters = node.text.toString();
-
   return sticky;
 }
 
-function renderObjectRecursively(data: any) {
-  if (isPrimitive(data)) {
-    const sticky = figma.createSticky();
-    sticky.text.characters = data.toString();
-
-    return [sticky];
-  } else {
-    const sections = Object.entries(data).map(([key, value]) => {
-      const section = figma.createSection();
-      section.name = key;
-
-      const childNodes = renderObjectRecursively(value);
-      const layoutFn = Array.isArray(data) ? getNextHorizontalTilePosition : getNextVerticalTilePosition;
-      appendAsTiles(section, childNodes, layoutFn);
-
-      return section;
-    });
-
-    return sections;
-  }
+function isDisplaySticky(node: DisplaySection | DisplaySticky): node is DisplaySticky {
+  return node.type === "Sticky";
+}
+function isDisplaySection(node: DisplaySection | DisplaySticky): node is DisplaySection {
+  return node.type === "Section";
 }
 
 function isPrimitive(data: any) {
