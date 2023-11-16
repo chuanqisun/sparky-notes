83037160fe496843d6433b80cacc846c3de5f8d8
chusun <chusun@microsoft.com>
2023-11-15 20:29:28 -0800

    update config ux

--- apps/impromptu/ui/lib/tools/chat/chat.ts
+++ apps/impromptu/ui/lib/tools/chat/chat.ts
@@ -1,5 +1,5 @@
 import { html } from "lit-html";
-import type { ChatMessage, SimpleChatProxy } from "plexchat";
+import type { ChatMessage, ModelName, SimpleChatProxy } from "plexchat";
 import { Observable, firstValueFrom, of, type Subject } from "rxjs";
 import type { MessageFromUI } from "../../../../types/message";
 
@@ -16,8 +16,53 @@ const defaultMessages = [
   },
 ];
 
+const defaultTemperature = 0;
+const defaultTokenLimit = 200;
+const defaultModel = "gpt-3.5";
+
 export const createChat = (config: { $chatProxy: Observable<SimpleChatProxy> }) => (props: { id: string; parsedBlob: any; $tx: Subject<MessageFromUI> }) => {
   const currentMessages = (props.parsedBlob.messages as { role: string; content: string }[]) ?? defaultMessages;
+
+  const { temperature = defaultTemperature, tokenLimit = defaultTokenLimit, model = defaultModel } = props.parsedBlob;
+
+  const handleTempChange = (e: InputEvent) => {
+    const temp = parseFloat((e.target as HTMLInputElement).value);
+    props.$tx.next({
+      setNodeBlob: {
+        id: props.id,
+        blob: JSON.stringify({
+          ...props.parsedBlob,
+          temperature: temp,
+        }),
+      },
+    });
+  };
+
+  const handleTokenLimitChange = (e: InputEvent) => {
+    const limit = parseInt((e.target as HTMLInputElement).value);
+    props.$tx.next({
+      setNodeBlob: {
+        id: props.id,
+        blob: JSON.stringify({
+          ...props.parsedBlob,
+          tokenLimit: limit,
+        }),
+      },
+    });
+  };
+
+  const handleModelChange = (e: InputEvent) => {
+    const model = (e.target as HTMLSelectElement).value;
+    props.$tx.next({
+      setNodeBlob: {
+        id: props.id,
+        blob: JSON.stringify({
+          ...props.parsedBlob,
+          model,
+        }),
+      },
+    });
+  };
 
   const handleInput = (e: InputEvent, index: number) => {
     const messageInput = (e.target as HTMLInputElement).value;
@@ -38,6 +83,9 @@ export const createChat = (config: { $chatProxy: Observable<SimpleChatProxy> }) 
     const chatProxy = await firstValueFrom(config.$chatProxy);
     const messageOutput = await chatProxy({
       messages: currentMessages as ChatMessage[],
+      temperature,
+      max_tokens: tokenLimit,
+      models: getModels(model),
     }).then((output) => output.choices[0].message.content ?? "Error");
 
     props.$tx.next({
@@ -67,6 +115,8 @@ export const createChat = (config: { $chatProxy: Observable<SimpleChatProxy> }) 
         id: props.id,
         blob: JSON.stringify({
           ...props.parsedBlob,
+          temperature: defaultTemperature,
+          tokenLimit: defaultTokenLimit,
           messages: [...defaultMessages],
         }),
       },
@@ -97,17 +147,25 @@ export const createChat = (config: { $chatProxy: Observable<SimpleChatProxy> }) 
   const handleSave = () => {};
 
   return of(html`
+    <label for="token-limit">Limit</label>
+    <input class="c-chat-config-input" id="token-limit" type="number" step="100" min="0" max="32000" .value=${tokenLimit} @input=${handleTokenLimitChange} />
+    <label for="temperature">Temp</label>
+    <input class="c-chat-config-input" id="temperature" type="number" step="0.1" min="0" max="1" .value=${temperature} @input=${handleTempChange} />
+    <label for="model">Model</label>
+    <select class="c-chat-config-select" id="model" .value=${model} @input=${handleModelChange}>
+      <option value="gpt-3.5">gpt-3.5</option>
+      <option value="gpt-4">gpt-4</option>
+    </select>
     <div class="c-chat-list">
       ${currentMessages.map(
         (message, index) => html`
           <div class="c-chat-entry">
             <div>
-              <b>${message.role === "assistant" ? "┗╸" : ""}${message.role}</b>${message.role === "user"
-                ? html`<button @click=${() => handleDelete(index, currentMessages.length)}>Delete</button>`
-                : ""}
+              <label for=${`chat-message-${index}`}><b>${message.role === "assistant" ? "┗╸" : ""}${message.role}</b></label
+              >${message.role === "user" ? html`<button @click=${() => handleDelete(index, currentMessages.length)}>Delete</button>` : ""}
             </div>
             <div class="u-auto-resize" data-resize-textarea-content=${message.content}>
-              <textarea .value=${message.content} @input=${(e: InputEvent) => handleInput(e, index)}></textarea>
+              <textarea id=${`chat-message-${index}`} .value=${message.content} @input=${(e: InputEvent) => handleInput(e, index)}></textarea>
             </div>
           </div>
         `
@@ -118,3 +176,14 @@ export const createChat = (config: { $chatProxy: Observable<SimpleChatProxy> }) 
     <button @click=${handleSave}>Save</button>
   `);
 };
+
+function getModels(model: string): ModelName[] {
+  switch (model) {
+    case "gpt-3.5":
+      return ["gpt-35-turbo", "gpt-35-turbo-16k"];
+    case "gpt-4":
+      return ["gpt-4", "gpt-4-32k"];
+    default:
+      return ["gpt-35-turbo", "gpt-35-turbo-16k"];
+  }
+}
