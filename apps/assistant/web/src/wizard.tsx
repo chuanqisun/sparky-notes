54cd6c00dc4a005fe2e83e93b737cb2701fd7014
chusun <chusun@microsoft.com>
2024-07-29 16:05:30 -0700

    template detection

--- apps/assistant/web/src/wizard.tsx
+++ apps/assistant/web/src/wizard.tsx
@@ -2,7 +2,7 @@ import type { MessageToFigma, MessageToWeb, RenderAutoLayoutItem, SearchNodeResu
 import { useAuth } from "@h20/auth/preact-hooks";
 import { getProxyToFigma } from "@h20/figma-tools";
 import { render } from "preact";
-import { useEffect, useRef, useState } from "preact/hooks";
+import { useCallback, useEffect, useRef, useState } from "preact/hooks";
 import { abortTask } from "./modules/copilot/abort";
 import { appInsights } from "./modules/telemetry/app-insights";
 import { ProgressBar } from "./styles/components/progress-bar";
@@ -17,10 +17,10 @@ window.focus();
 appInsights.trackPageView();
 
 interface TemplateLibrary {
-  threadTemplate?: SearchNodeResult | null;
-  userMessageTemplate?: SearchNodeResult | null;
-  spinnerTemplate?: SearchNodeResult | null;
-  assistantMessageTemplates: (SearchNodeResult & { displayName: string })[];
+  threadTemplates: SearchNodeResult[];
+  userTemplates: SearchNodeResult[];
+  spinnerTemplates: SearchNodeResult[];
+  copilotTemplates: (SearchNodeResult & { displayName: string })[];
 }
 
 function App() {
@@ -29,7 +29,12 @@ function App() {
   });
 
   const [selection, setSelection] = useState<SelectionSummary | null>(null);
-  const [templateLibrary, setTemplateLibrary] = useState<TemplateLibrary>({ assistantMessageTemplates: [] });
+  const [templateLibrary, setTemplateLibrary] = useState<TemplateLibrary>({
+    copilotTemplates: [],
+    threadTemplates: [],
+    userTemplates: [],
+    spinnerTemplates: [],
+  });
 
   // Figma RPC
   useEffect(() => {
@@ -59,20 +64,20 @@ function App() {
 
   const handleLoadTemplates = async () => {
     const { searchNodesByNamePattern } = await proxyToFigma.request({
-      searchNodesByNamePattern: String.raw`@(assistant-message-template\/.+)|(thread)|(user-message-template)|(spinner-template)`,
+      searchNodesByNamePattern: String.raw`@(copilot-template\/.+)|(thread)|(user-template)|(spinner-template)`,
     });
     if (!searchNodesByNamePattern) return;
 
     setTemplateLibrary((prev) => ({
       ...prev,
-      threadTemplate: searchNodesByNamePattern.find((p) => p.name === "@thread") ?? null,
-      userMessageTemplate: searchNodesByNamePattern.find((p) => p.name === "@user-message-template") ?? null,
-      spinnerTemplate: searchNodesByNamePattern.find((p) => p.name === "@spinner-template") ?? null,
-      assistantMessageTemplates: searchNodesByNamePattern
-        .filter((p) => p.name.startsWith("@assistant-message-template/"))
+      threadTemplates: searchNodesByNamePattern.filter((p) => p.name === "@thread"),
+      userTemplates: searchNodesByNamePattern.filter((p) => p.name === "@user-template"),
+      spinnerTemplates: searchNodesByNamePattern.filter((p) => p.name === "@spinner-template"),
+      copilotTemplates: searchNodesByNamePattern
+        .filter((p) => p.name.startsWith("@copilot-template/"))
         .map((p) => ({
           ...p,
-          displayName: p.name.replace("@assistant-message-template/", ""),
+          displayName: p.name.replace("@copilot-template/", ""),
         }))
         .sort((a, b) => a.displayName.localeCompare(b.displayName)),
     }));
@@ -91,8 +96,13 @@ function App() {
     element.value = "";
   };
 
+  const handleLocateNodeByNames = useCallback((names: string[]) => {
+    handleLoadTemplates();
+    handleZoomNodeIntoView(names);
+  }, []);
+
   const userMessageTextAreaRef = useRef<HTMLTextAreaElement>(null);
-  const assistantMesageTextAreaRef = useRef<HTMLTextAreaElement>(null);
+  const copilotMessageVariableValueRef = useRef<HTMLTextAreaElement>(null);
 
   return (
     <>
@@ -104,115 +114,154 @@ function App() {
             <a href="/index.html">← Back to search</a>
           </nav>
           <section class="c-module-stack__section">
-            <h2>Utils</h2>
-            <menu>
-              <button onClick={() => handleLoadTemplates()}>Load templates</button>
-              <button onClick={() => handleRenderItem({ containerName: "@thread", clear: true })}>Reset chat</button>
-            </menu>
+            <header class="c-split-header">
+              <h2>Thread</h2>
+              <span>
+                {templateLibrary.threadTemplates?.length ? (
+                  <a
+                    href="javascript:void(0)"
+                    onClick={() => handleLocateNodeByNames(templateLibrary.threadTemplates.map((t) => t.name))}
+                    title="Click to locate"
+                  >
+                    ❖@thread
+                  </a>
+                ) : (
+                  <a
+                    href="javascript:void(0)"
+                    onClick={() => handleLocateNodeByNames(templateLibrary.threadTemplates.map((t) => t.name))}
+                    title={`Component or Frame named "@thread" not found. Click to re-scan`}
+                  >
+                    ❖@thread ⚠️
+                  </a>
+                )}
+              </span>
+            </header>
+            <button onClick={() => handleRenderItem({ containerName: "@thread", clear: true })}>Clear</button>
           </section>
           <section class="c-module-stack__section">
-            <h2>User message</h2>
+            <header class="c-split-header">
+              <h2>User message</h2>
+              <span>
+                {templateLibrary.userTemplates ? (
+                  <a
+                    href="javascript:void(0)"
+                    onClick={() => handleLocateNodeByNames(templateLibrary.userTemplates.map((t) => t.name))}
+                    title="Click to locate"
+                  >
+                    ❖@user-template
+                  </a>
+                ) : (
+                  <a
+                    href="javascript:void(0)"
+                    onClick={() => handleLocateNodeByNames(templateLibrary.userTemplates.map((t) => t.name))}
+                    title={`Component or Frame named "@user-template" not found. Click to re-scan`}
+                  >
+                    ❖@user-template ⚠️
+                  </a>
+                )}
+              </span>
+            </header>
             <textarea rows={6} ref={userMessageTextAreaRef}></textarea>
-            <menu>
-              <button
-                onClick={() =>
-                  handleRenderItem({
-                    containerName: "@thread",
-                    templateName: "@user-message-template",
-                    replacements: {
-                      content: userMessageTextAreaRef.current?.value ?? "",
-                    },
-                  }).then(() => clearTextAreaElement(userMessageTextAreaRef.current))
-                }
-              >
-                Append
-              </button>
-              <button
-                onClick={() =>
-                  handleRenderItem({
-                    containerName: "@thread",
-                    templateName: "@spinner-template",
-                    clear: "@spinner-instance",
-                  })
-                }
-              >
-                Show spinner
-              </button>
-            </menu>
+            <button
+              onClick={() =>
+                handleRenderItem({
+                  containerName: "@thread",
+                  templateName: "@user-template",
+                  replacements: {
+                    content: userMessageTextAreaRef.current?.value ?? "",
+                  },
+                }).then(() => clearTextAreaElement(userMessageTextAreaRef.current))
+              }
+            >
+              Append
+            </button>
           </section>
+
           <section class="c-module-stack__section">
-            <h2>Assistant message</h2>
-            {templateLibrary.assistantMessageTemplates.map((template) => (
+            <header class="c-split-header">
+              <h2>Spinner</h2>
+              <span>
+                {templateLibrary.spinnerTemplates.length ? (
+                  <a
+                    href="javascript:void(0)"
+                    onClick={() => handleLocateNodeByNames(templateLibrary.spinnerTemplates.map((t) => t.name))}
+                    title="Click to locate"
+                  >
+                    ❖@spinner-template
+                  </a>
+                ) : (
+                  <a
+                    href="javascript:void(0)"
+                    onClick={() => handleLocateNodeByNames(templateLibrary.spinnerTemplates.map((t) => t.name))}
+                    title={`Component or Frame named "@spinner-template" not found. Click to re-scan`}
+                  >
+                    ❖@spinner-template ⚠️
+                  </a>
+                )}
+              </span>
+            </header>
+            <button
+              onClick={() =>
+                handleRenderItem({
+                  containerName: "@thread",
+                  templateName: "@spinner-template",
+                  clear: "@spinner-instance",
+                })
+              }
+            >
+              Show spinner
+            </button>
+          </section>
+
+          <section class="c-module-stack__section">
+            <div class="c-split-header">
+              <h2>Copilot message</h2>
+              <span>
+                {templateLibrary.copilotTemplates.length ? (
+                  <a
+                    href="javascript:void(0)"
+                    onClick={() => handleLocateNodeByNames(templateLibrary.copilotTemplates.map((t) => t.name))}
+                    title="Click to locate"
+                  >
+                    ❖@copilot-template/*
+                  </a>
+                ) : (
+                  <a
+                    href="javascript:void(0)"
+                    onClick={() => handleLocateNodeByNames(templateLibrary.copilotTemplates.map((t) => t.name))}
+                    title={`Components or Frames named "@copilot-template/<replace_with_real_name>" not found. Click to reload`}
+                  >
+                    ❖@copilot-template/* ⚠️
+                  </a>
+                )}
+              </span>
+            </div>
+            {templateLibrary.copilotTemplates.map((template) => (
               <button
                 onClick={() =>
                   handleRenderItem({
                     containerName: "@thread",
                     templateName: template.name,
                     clear: "@spinner-instance",
-                  })
+                    replacements: {
+                      content: copilotMessageVariableValueRef.current?.value ?? "",
+                    },
+                  }).then(() => clearTextAreaElement(copilotMessageVariableValueRef.current))
                 }
               >
                 {template.displayName}
               </button>
             ))}
-            <details>
-              <summary>Custom</summary>
+            <details open>
+              <summary>Variable value</summary>
               <div class="c-module-stack__section c-module-stack__no-padding">
-                <textarea rows={6} ref={assistantMesageTextAreaRef}></textarea>
-                <button
-                  onClick={() =>
-                    handleRenderItem({
-                      containerName: "@thread",
-                      templateName: "@assistant-message-template",
-                      clear: "@spinner-instance",
-                      replacements: {
-                        content: assistantMesageTextAreaRef.current?.value ?? "",
-                      },
-                    }).then(() => clearTextAreaElement(assistantMesageTextAreaRef.current))
-                  }
-                >
-                  Append
-                </button>
+                <textarea
+                  rows={6}
+                  ref={copilotMessageVariableValueRef}
+                  placeholder="Enter any text to replace the {{content}} string in the Copilot message template."
+                ></textarea>
               </div>
             </details>
-          </section>
-
-          <section class="c-module-stack__section">
-            <h2>Special components</h2>
-            <table>
-              <tr>
-                <td>{templateLibrary.threadTemplate ? "✅" : "⚠️"}</td>
-                <td>
-                  {templateLibrary.threadTemplate ? (
-                    <a href="#" onClick={() => handleZoomNodeIntoView([templateLibrary.threadTemplate!.name])}>
-                      ❖@thread
-                    </a>
-                  ) : (
-                    <span>❖@thread</span>
-                  )}{" "}
-                </td>
-              </tr>
-              <tr>
-                <td>{templateLibrary.userMessageTemplate ? "✅" : "⚠️"}</td>
-                <td>
-                  {templateLibrary.userMessageTemplate ? (
-                    <a href="#" onClick={() => handleZoomNodeIntoView([templateLibrary.userMessageTemplate!.name])}>
-                      ❖@user-message-template
-                    </a>
-                  ) : (
-                    <span>❖@user-message-template</span>
-                  )}
-                </td>
-              </tr>
-              <tr>
-                <td>{templateLibrary.assistantMessageTemplates?.length ? "✅" : "⚠️"}</td>
-                <td>{`❖@assistant-message-template/<name>`}</td>
-              </tr>
-              <tr>
-                <td>{templateLibrary.spinnerTemplate ? "✅" : "⚠️"}</td>
-                <td>❖@spinner-template</td>
-              </tr>
-            </table>
           </section>
         </div>
       )}
